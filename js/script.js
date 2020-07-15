22b037d1bf72a1b50a71d1a09edb361292840ad2
ISHIDA Naoto <naoto@isnot.jp>
2020-07-15 22:26:42 +0900

    Site updated: 2020-07-15 22:26:42

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
   // Mobile nav
   const container = document.getElementById('container');
 
-  document.getElementById('main-nav-toggle').addEventListener('click', () => {
+  document.getElementById('main-nav-toggle').addEventListener('click', (e) => {
     container.classList.toggle('mobile-nav-on');
+    e.stopPropagation();
   }, false);
 
-  document.getElementById('wrap').addEventListener('click', () => {
+  document.getElementById('wrap').addEventListener('click', (e) => {
     if (!container.classList.contains('mobile-nav-on')) return;
     container.classList.remove('mobile-nav-on');
   }, false);
